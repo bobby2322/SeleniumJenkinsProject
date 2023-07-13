18f8736bf5c8908e0f0f0c704922f0aa2828731b
Srinivas <bobby2322@gmail.com>
2023-07-13 18:43:38 +0530

    param addition

--- src/test/java/testNG/TestBase.java
+++ src/test/java/testNG/TestBase.java
@@ -5,16 +5,19 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestBase {	
 	
 	static WebDriver driver;
-	String browser = "Chrome";
+//	String browser = "Chrome";
 	
+	//coming from pom.xml
+	@Parameters("Browser")
 	@BeforeSuite
-	public void getDriver() {
+	public void getDriver(String browser) {
 		
 		if(browser.equals("Chrome")) {
 			WebDriverManager.chromedriver().setup();
